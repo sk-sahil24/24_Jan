0751cffa1350f8717abe263e338044d8ab841e09
sk-sahil24 <98327238+sk-sahil24@users.noreply.github.com>
2022-01-27 19:34:55 +0530

    Create Assertion.sol

---
+++ Solidity/Assertion.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.0;
+
+contract Assertion {
+    
+
+    function get() public view {
+        require(true == false, "Reasoooon");
+    }
+}
